e5e96872df1adea0566635927f67548c46c49fdb
edigavenkat7-glitch <edigavenkat7@gmail.com>
2025-12-11 17:20:30 +0530

    Update sql-question-2.sql

--- sql-question-2.sql
+++ sql-question-2.sql
@@ -1,40 +1,43 @@
-/* practise all joins with below data set , you will get handson experience with joins */
+/* practise all joins with below data set , you will get handson experience with joins and syntax  */
 
-create table ezecash.t1(s_no number);
-create table ezecash.t2(s_no number);
+create table t1(s_no number);
+create table t2(s_no number);
 
-insert all into ezecash.t1 values(1)
-into ezecash.t1 values(1)
-into ezecash.t1 values(1)
-into ezecash.t1 values(2)
-into ezecash.t1 values(3)
-into ezecash.t1 values(3)
-into ezecash.t1 values(3)
+insert all into t1 values(1)
+into t1 values(1)
+into t1 values(1)
+into t1 values(2)
+into t1 values(3)
+into t1 values(3)
+into t1 values(3)
 select * from dual;
 
-insert all into ezecash.t2 values(1)
-into ezecash.t2 values(1)
-into ezecash.t2 values(2)
-into ezecash.t2 values(2)
-into ezecash.t2 values(4)
-into ezecash.t2 values(null)
+insert all into t2 values(1)
+into t2 values(1)
+into t2 values(2)
+into t2 values(2)
+into t2 values(4)
+into t2 values(null)
 select * from dual;
 
 #inner join 
 
-select * from ezecash.t1 inner join ezecash.t2 on t1.s_no=t2.s_no;
+select * from t1 inner join t2 on t1.s_no=t2.s_no;
 
 #left join 
 
-select * from ezecash.t1 left join ezecash.t2 on t1.s_no=t2.s_no;
+select * from t1 left join t2 on t1.s_no=t2.s_no;
 
 #right join 
-select * from ezecash.t1 right join ezecash.t2 on t1.s_no=t2.s_no;
+select * from t1 right join t2 on t1.s_no=t2.s_no;
 
 #self  join
-select * from ezecash.t1 a join ezecash.t1 b on a.s_no=b.s_no;
+select * from t1 a join t1 b on a.s_no=b.s_no;
+
+#cross join 
+  select * from t1 a , t2 ;
 
 #full outer join 
-select * from ezecash.t1 a full outer join ezecash.t1 b on a.s_no=b.s_no;
+select * from t1 a full outer join t1 b on a.s_no=b.s_no;
 
 
